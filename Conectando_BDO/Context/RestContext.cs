ce5558b0e50273f0cd0cbc4274e4cd477ba65926
Gabriel Maurici <gabriel.maurici@hotmail.com>
2021-08-10 14:00:46 -0300

    Add instruções na class RestContext

--- Conectando_BDO/Context/RestContext.cs
+++ Conectando_BDO/Context/RestContext.cs
@@ -7,10 +7,16 @@ using System.Web;
 
 namespace Conectando_BDO.Context
 {
+    // Install-Package EntityFramework
+    // Criar connectionString no web.config
+    // Enable-Migration
+    // Adicionar uma prop tipo DbSet neste arquivo de Contexto
+    // Add-Migration (Para cada novo model)
+    // Update-Database - Alterar o banco
+
     public class RestContext : DbContext
     {
         public DbSet<Vinhos> Vinhos { get; set; }
-
         public RestContext() : base("RestConnection")
         {
             
